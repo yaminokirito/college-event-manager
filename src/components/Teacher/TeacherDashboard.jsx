bbdd60caf52df94821e5da5688f79c3a30cc2782
yaminokirito <yohan442004@gmail.com>
2025-12-31 14:01:48 +0530

    updated uploads and dash

--- src/components/Teacher/TeacherDashboard.jsx
+++ src/components/Teacher/TeacherDashboard.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { collection, getDocs, updateDoc, doc } from "firebase/firestore";
-import { db } from "../firebase";
+import { db } from ".../firebase";
 
 export default function TeacherDashboard() {
   const [bookings, setBookings] = useState([]);
