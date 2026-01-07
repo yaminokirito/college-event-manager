929d057cd63adfe04683e298f6854bb5442354cf
yaminokirito <yohan442004@gmail.com>
2026-01-07 11:06:40 +0530

    view pdf working

--- src/components/Teacher/EventReports.jsx
+++ src/components/Teacher/EventReports.jsx
@@ -47,13 +47,13 @@ export default function EventReports() {
             </div>
 
             <a
-              href={r.fileUrl}
-              target="_blank"
-              rel="noreferrer"
-              className="px-3 py-1 bg-blue-500 text-black rounded"
-            >
-              View PDF
-            </a>
+  href={r.pdfUrl}
+  target="_blank"
+  rel="noreferrer"
+  className="px-3 py-1 bg-blue-500 text-black rounded"
+>
+  View PDF
+</a>
           </div>
         ))
       )}
